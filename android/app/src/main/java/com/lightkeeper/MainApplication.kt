694c4805e81ba8b3eed888912a6474dc319ece5b
Youssef-S-Negm <youssef.negm2001@gmail.com>
2024-12-16 16:19:35 +0200

    feat: add NativeLightSensorPackage

--- android/app/src/main/java/com/lightkeeper/MainApplication.kt
+++ android/app/src/main/java/com/lightkeeper/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.lightkeeper.nativemodule.NativeLightSensorPackage
 import com.lightkeeper.nativemodule.NativeTorchPackage
 
 class MainApplication : Application(), ReactApplication {
@@ -22,6 +23,7 @@ class MainApplication : Application(), ReactApplication {
                     // Packages that cannot be autolinked yet can be added manually here, for example:
                     // add(MyReactNativePackage())
                     add(NativeTorchPackage())
+                    add(NativeLightSensorPackage())
                 }
 
             override fun getJSMainModuleName(): String = "index"
